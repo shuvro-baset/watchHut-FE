4b05b187949cd42c7f1a1fa7c5914be255c2d161
shuvro baset <md.abdul.baset75@gmail.com>
2021-11-12 18:47:32 +0600

    make admin

--- src/pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { Alert, Form } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Alert, Container, Form, Row } from 'react-bootstrap';
 import useAuth from '../../../hooks/useAuth';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('');
     const [success, setSuccess] = useState(false);
     const { token } = useAuth();
+    const [users, setUsers] = useState([])
 
     const handleOnBlur = e => {
         setEmail(e.target.value);
@@ -30,15 +31,35 @@ const MakeAdmin = () => {
             })
         e.preventDefault()
     }
+    // getting watches information
+    useEffect(() => {
+        fetch('http://localhost:5000/all-users')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+    }, [])
+
+
     return (
-        <div>
-            <h3>make new admin from here........</h3>
-                <Form onSubmit={handleMakeAdmin}>
-                    <input onBlur={handleOnBlur} type="email" placeholder="name" /> <br />
-                    <button type="submit">Register</button>
-                </Form>
-            {success && <Alert severity="success">Made Admin successfully!</Alert>}
-        </div>
+        <Container className="my-5">
+            <Row>
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                <div className="mt-3 mb-5">
+                    <h2 className="div-head">Make Admin </h2>
+                    <div className="underline"></div>
+                </div>
+                    <Form onSubmit={handleMakeAdmin} className="login-div">
+                        <select onBlur={handleOnBlur}>
+                           { users.map(user =>
+                            <option value={user.email}> {user.email} </option>
+                           )}
+                        </select>
+                        <br />
+                        <button className="btn btn-order my-3" type="submit">submit</button>
+                    </Form>
+                {success && <Alert severity="success">Made Admin successfully!</Alert>}
+            </div>
+            </Row>
+        </Container>
     );
 };
 
